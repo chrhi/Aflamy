6717b84129de5cf9a9f4fb9c2edd47854c8791ce
abdellah <Abdellah.chehri14@gmail.com>
2024-08-08 11:20:57 +0200

    initial commit

---
+++ src/components/bnnner.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+interface BnnnerProps {}
+
+const Bnnner: FC = ({}) => {
+  return <div>bnnner</div>;
+};
+
+export default Bnnner;
---
+++ src/components/movie-card.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+interface MovieCardProps {}
+
+const MovieCard: FC = ({}) => {
+  return <div>movie-card</div>;
+};
+
+export default MovieCard;
