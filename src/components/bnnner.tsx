bd3510b63ed809b77f03612a373011ee362153cf
abdellah <Abdellah.chehri14@gmail.com>
2024-08-14 06:41:59 +0200

    push a changes to main

--- src/components/bnnner.tsx
+++ src/components/bnnner.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import MaxWidthWrapper from "./max-width-wrapper";
 import Image from "next/image";
 import {
   Carousel,
@@ -8,18 +7,15 @@ import {
   CarouselItem,
   type CarouselApi,
 } from "@/components/ui/carousel";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Movie } from "@/types";
 
 export default function Banner({ moviesData }: { moviesData: Movie[] }) {
-  //https://image.tmdb.org/t/p/w500/
-  //https://image.tmdb.org/t/p/original/
-
   return (
     <div className="w-full overflow-hidden relative h-fit my-8">
       <Carousel className="w-full ">
         <CarouselContent>
-          {moviesData.slice(0, 5).map((item) => {
+          {moviesData.slice(5, 10).map((item) => {
             return (
               <CarouselItem key={item?.id}>
                 <div className="w-full h-[400px] 2xl:h-[500px] relative p-10 ">
